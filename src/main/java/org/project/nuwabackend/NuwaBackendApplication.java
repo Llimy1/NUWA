dd559a10906178ef8f085f79f8bf75423983c29b
Llimy1 <alsgur990104@gmail.com>
2024-01-28 20:58:14 +0900

    :memo: setting project

---
+++ src/main/java/org/project/nuwabackend/NuwaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.project.nuwabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuwaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuwaBackendApplication.class, args);
+    }
+
+}
