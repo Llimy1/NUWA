4d4ee74b52547b8dfdf7a2c462707dc28aff8a1d
Llimy1 <alsgur990104@gmail.com>
2024-02-14 23:14:48 +0900

    :coffin: Delete Category

--- src/main/java/org/project/nuwabackend/repository/jpa/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package org.project.nuwabackend.repository.jpa;
-
-import org.project.nuwabackend.domain.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
