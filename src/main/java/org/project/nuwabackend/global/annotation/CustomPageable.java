badbafd0c57354444ec29369ed3a2d9180b04a48
Llimy1 <alsgur990104@gmail.com>
2024-02-20 22:44:55 +0900

    :sparkles: page size change

--- src/main/java/org/project/nuwabackend/global/annotation/CustomPageable.java
+++ src/main/java/org/project/nuwabackend/global/annotation/CustomPageable.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CustomPageable {
     int page() default 1;
-    int size() default 20;
+    int size() default 10;
     String sortBy() default "createdAt";
 }
